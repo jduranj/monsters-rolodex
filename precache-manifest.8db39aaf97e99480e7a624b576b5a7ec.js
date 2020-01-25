5bd251345b33edde476ff31580a43976176ecc0d
Jesús Durán <47229820+jduranj@users.noreply.github.com>
2020-01-24 22:45:30 -0600

    Updates

---
+++ precache-manifest.8db39aaf97e99480e7a624b576b5a7ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133ef35f7ac0d823074b9aa6bf99087d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d1e9ab4ac019830d5a3",
+    "url": "/monsters-rolodex/static/css/main.54a64288.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d1e9ab4ac019830d5a3",
+    "url": "/monsters-rolodex/static/js/main.5e4faac1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
